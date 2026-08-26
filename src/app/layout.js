535db5011946fd7852f09b3931f614ad05a36363
doudouman01 <goutercalaydou@gmail.com>
2026-08-25 19:06:33 -0700

    Mise a jour 25/08/2026 19:06:33,15

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
   title: 'Solutions Directes Pro — Des solutions directes, partout dans le monde',
